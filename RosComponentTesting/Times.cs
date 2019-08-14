da3c91c665bc5c9247d364c36f50cccaaa0fb796
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2019-08-14 14:14:58 +0200

    Add Times Tests

--- RosComponentTesting/Times.cs
+++ RosComponentTesting/Times.cs
@@ -7,9 +7,22 @@ namespace RosComponentTesting
         public static readonly Times Never = new Times(0, 0);
         public static readonly Times Once = new Times(1, 1);
 
+        public static Times AtLeast(int value)
+        {
+            if (value < 0) throw new ArgumentOutOfRangeException(nameof(value));
+            
+            return AtLeast((uint) value);
+        }
+        
         public static Times AtLeast(uint value)
         {
             return new Times((uint) value, uint.MaxValue);
+        }
+
+        public static Times AtMost(int value)
+        {
+            if (value < 0) throw new ArgumentOutOfRangeException(nameof(value));
+            return AtMost((uint) value);
         }
 
         public static Times AtMost(uint value)
@@ -17,17 +30,41 @@ namespace RosComponentTesting
             return new Times(0, value);
         }
 
+        public static Times Exactly(int value)
+        {
+            if (value < 0) throw new ArgumentOutOfRangeException(nameof(value));
+            
+            return Exactly((uint) value);
+        }
+
         public static Times Exactly(uint value)
         {
             return new Times(value, value);
         }
-        
+
+        public static Times Between(int min, int max)
+        {
+            if (min < 0) throw new ArgumentOutOfRangeException(nameof(min));
+            if (max < 0) throw new ArgumentOutOfRangeException(nameof(max));
+
+            return Between((uint) min, (uint) max);
+        }
+
+        public static Times Between(uint min, uint max)
+        {
+            if (min > max)
+            {
+                throw new ArgumentOutOfRangeException((nameof(min)));
+            }
+            
+            return new Times(min, max);
+        }
         
         public uint Min { get; }
         
         public uint Max { get; }
 
-        public Times(uint min, uint max)
+        private Times(uint min, uint max)
         {
             Min = min;
             Max = max;
