560fcb2d75a6048ebb3706990340a6ffe2ae5da8
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2020-12-04 21:41:57 +0100

    Fixed framework version issue

--- Examples.TurtleSimTests/RosApplication.cs
+++ Examples.TurtleSimTests/RosApplication.cs
@@ -6,12 +6,12 @@ namespace Examples.TurtleSimTests
 {
     public interface IRosApplication
     {
-        public void CallService(string serviceName, TeleportAbsolute.Request request);
+        void CallService(string serviceName, TeleportAbsolute.Request request);
 
-        public TServiceResponse CallService<TServiceResponse>(string serviceName, TeleportAbsolute.Request request);
+        TServiceResponse CallService<TServiceResponse>(string serviceName, TeleportAbsolute.Request request);
         
         void Advertise(string topic, Type messageType);
 
-        public void Publish(string topic, RosMessage message);
+        void Publish(string topic, RosMessage message);
     }
 }
