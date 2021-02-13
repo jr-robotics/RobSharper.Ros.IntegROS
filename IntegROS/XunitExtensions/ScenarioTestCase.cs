506a5165dcce7441f8d223e08ecd0b8be2ef7d75
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2021-02-13 13:53:45 +0100

    Extracted class to file

--- IntegROS/XunitExtensions/ScenarioTestCase.cs
+++ IntegROS/XunitExtensions/ScenarioTestCase.cs
@@ -9,36 +9,6 @@ using Xunit.Sdk;
 
 namespace IntegROS.XunitExtensions
 {
-    public class MultipleScenariosTestCase : XunitTestCase
-    {
-        protected internal new TestMethodDisplay DefaultMethodDisplay => base.DefaultMethodDisplay;
-        protected internal new TestMethodDisplayOptions DefaultMethodDisplayOptions => base.DefaultMethodDisplayOptions;
-        
-
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        [Obsolete("Called by the de-serializer; should only be called by deriving classes for de-serialization purposes")]
-        public MultipleScenariosTestCase() {}
-
-        public MultipleScenariosTestCase(IMessageSink diagnosticMessageSink,
-            TestMethodDisplay defaultMethodDisplay,
-            TestMethodDisplayOptions defaultMethodDisplayOptions,
-            ITestMethod testMethod,
-            object[] testMethodArguments = null) : base(diagnosticMessageSink, defaultMethodDisplay,
-            defaultMethodDisplayOptions, testMethod,
-            testMethodArguments)
-        {
-        }
-        
-        public override Task<RunSummary> RunAsync(IMessageSink diagnosticMessageSink, IMessageBus messageBus, object[] constructorArguments,
-            ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource)
-        {
-            var runner = new MultipleScenariosTestCaseRunner(this, DisplayName, SkipReason, constructorArguments,
-                diagnosticMessageSink, messageBus, aggregator, cancellationTokenSource);
-
-            return runner.RunAsync();
-        }
-    }
-    
     public class ScenarioTestCase : XunitTestCase
     {
         private IScenarioIdentifier _scenarioIdentifier;
