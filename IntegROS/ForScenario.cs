1afe8e8ba454576ee7104c23d14fa41ae1b95318
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2021-02-15 10:39:34 +0100

    Removed expectation from scenario base class.

--- IntegROS/ForScenario.cs
+++ IntegROS/ForScenario.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using FluentAssertions;
 using IntegROS.Scenarios;
 
 namespace IntegROS
@@ -9,26 +8,9 @@ namespace IntegROS
         public IScenario Scenario { get; internal set; }
 
         public IEnumerable<IRecordedMessage> Messages => Scenario?.Messages;
-        
-        public bool ShouldContainMessages { get; set; } = true;
 
         public ForScenario()
         {
-        }
-        
-        public ForScenario(bool shouldContainMessages)
-        {
-            ShouldContainMessages = shouldContainMessages;
-        }
-
-        [ExpectThat]
-        public void Should_contain_messages()
-        {
-            Scenario.Should().NotBeNull("scenario should be available");
-            Scenario.Messages.Should().NotBeNull("it should not be null");
-
-            if (ShouldContainMessages)
-                Scenario.Messages.Should().NotBeEmpty("it should contain messages");
         }
     }
 }
