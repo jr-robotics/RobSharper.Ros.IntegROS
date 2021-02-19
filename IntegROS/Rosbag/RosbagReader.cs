9adde53d573c7dcd07efb02c0e9ea0418be1fb9a
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2021-02-19 15:00:47 +0100

    Add support for UML Robotics ROS messages

--- IntegROS/Rosbag/RosbagReader.cs
+++ IntegROS/Rosbag/RosbagReader.cs
@@ -1,3 +1,4 @@
+using RobSharper.Ros.Adapters.UmlRobotics;
 using RobSharper.Ros.MessageEssentials;
 using RobSharper.Ros.MessageEssentials.Serialization;
 
@@ -6,6 +7,16 @@ namespace IntegROS.Rosbag
     public static class RosbagReader
     {
         // TODO: Avoid default hardcoded initialization
-        public static IRosbagReader Instance { get; set; } = new RobSharperRosbagReaderAdapter(new RosMessageSerializer(new MessageTypeRegistry()));
+        public static IRosbagReader Instance { get; set; }
+
+        static RosbagReader()
+        {
+            var serializer = new RosMessageSerializer(new MessageTypeRegistry());
+            
+            serializer.MessageFormatters.Add(new UmlRoboticsRosMessageFormatter());
+            serializer.MessageTypeRegistry.RosMessageTypeInfoFactories.Add(new UmlRoboticsMessageTypeInfoFactory());
+            
+            Instance = new RobSharperRosbagReaderAdapter(serializer);
+        }
     }
 }
