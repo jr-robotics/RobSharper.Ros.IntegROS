00f815ecba85263126c85abceca7c0f1f178519c
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2021-03-10 14:44:59 +0100

    Removed direct dependency from IntegROS to UML.Robotics.Ros.

---
+++ RobSharper.Ros.IntegROS.Tests.WithUmlRoboticsSupport/UmlRoboticsAdapterTypeLoaderTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using RobSharper.Ros.IntegROS.Ros.MessageEssentials;
+using Xunit;
+
+namespace RobSharper.Ros.IntegROS.Tests.WithUmlRoboticsSupport
+{
+    /// <summary>
+    /// These tests will fail if RobSharper.Ros.Adapters.UmlRobotics
+    /// is not installed in the project
+    /// </summary>
+    public class UmlRoboticsAdapterTypeLoaderTests
+    {
+        [Fact]
+        public void UmlRobotics_Adapter_is_available()
+        {
+            var exists = UmlRoboticsAdapterTypeLoader.AssemblyAvailable;
+
+            exists.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Can_create_formatter()
+        {
+            var item = UmlRoboticsAdapterTypeLoader.CreateFormatter();
+
+            item.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Can_create_type_info_factory()
+        {
+            var item = UmlRoboticsAdapterTypeLoader.CreateTypeInfoFactory();
+
+            item.Should().NotBeNull();
+        }
+    }
+}
