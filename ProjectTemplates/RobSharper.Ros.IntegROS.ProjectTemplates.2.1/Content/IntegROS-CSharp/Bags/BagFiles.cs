ba8ef6982b0d13704576207b923d3e5d56b3a177
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2021-03-11 18:56:51 +0100

    Add Templates for .Net 6 and .Net Core 2.1

---
+++ ProjectTemplates/RobSharper.Ros.IntegROS.ProjectTemplates.2.1/Content/IntegROS-CSharp/Bags/BagFiles.cs
@@ -0,0 +1,9 @@
+namespace Company.TestProject1
+{
+    public class BagFiles
+    {
+        private const string BasePath = "Bags/";
+
+        public const string MyRecordedBag = BasePath + "my-recorded-example-bag.bag";
+    }
+}
