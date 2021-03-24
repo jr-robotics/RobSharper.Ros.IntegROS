f318545ce99398d8be5aef1ccb84e30b8e7a6810
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2021-03-24 15:36:07 +0100

    Add Comment

--- RobSharper.Ros.IntegROS.Tests.WithUmlRoboticsSupport/JrFactAttribute.cs
+++ RobSharper.Ros.IntegROS.Tests.WithUmlRoboticsSupport/JrFactAttribute.cs
@@ -4,6 +4,9 @@ namespace RobSharper.Ros.IntegROS.Tests.WithUmlRoboticsSupport
 {
     public class JrFactAttribute : FactAttribute
     {
+        /// <summary>
+        /// Tests are skipped, if compiler property JRINTERNAL=True is not set.
+        /// </summary>
         public JrFactAttribute() : base()
         {
 #if !JRINTERNAL
